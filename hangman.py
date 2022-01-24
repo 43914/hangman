9cdf938a8936fd5f8ef534b38f922d1ffe94a473
43914 <98344017+43914@users.noreply.github.com>
2022-01-24 09:33:01 -0800

    Python file
    First python files. Includes dictionary with (almost) every word in US dictionary and the actual python file.

---
+++ hangman.py
@@ -0,0 +1,5 @@
+# = comment
+## = code
+import random
+word_list = open("dictionary.txt").readlines() #makes a list of every word in the dictionary.txt file
+##print(random.choice(word_list)) #prints a random word from US dictionary (61k+ words)
